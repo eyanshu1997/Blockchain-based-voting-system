7dbd10f37fa13ff2ff50831eafa8adf84fb5c9c8
Eyanshu Gupta <eyanshu1997@gmail.com>
2019-04-17 23:06:48 +0530

    third commit

--- gui.py
+++ gui.py
@@ -6,31 +6,28 @@ def focus1(event):
     course_field.focus_set() 
   
   
-
-
-	
+  
+re = tk.Tk() 
+re.title('voting system')
+re.configure(background='light green')
+re.geometry("500x300")
 def avoter():
-	root = Tk()
-	root.configure(background='light green')
-	root.title("registration form")
-	root.geometry("500x300")
+	heading = Label(re, text="Form", bg="light green")
+	name = Label(re, text="Name", bg="light green")
+	pas = Label(re, text="voter pass", bg="light green")
 	
-	heading = Label(root, text="Form", bg="light green")
-	name = Label(root, text="Name", bg="light green")
-	pas = Label(root, text="voter pass", bg="light green")
+	heading.grid(row=1, column=1)
+	name.grid(row=2, column=0)
+	pas.grid(row=3, column=0)
 	
-	heading.grid(row=0, column=1)
-	name.grid(row=1, column=0)
-	pas.grid(row=2, column=0)
-	
-	name_field = Entry(root)
-	pass_field = Entry(root)
+	name_field = Entry(re)
+	pass_field = Entry(re)
 	
 	name_field.bind("<Return>", focus1)
 	pass_field.bind()
 	
-	name_field.grid(row=1, column=1, ipadx="100")
-	pass_field.grid(row=2, column=1, ipadx="100") 
+	name_field.grid(row=2, column=1, ipadx="50")
+	pass_field.grid(row=3, column=1, ipadx="50") 
 
 	def calc():
 		et=name_field.get()
@@ -38,13 +35,10 @@ def avoter():
 		print et
 		print xt
 		
-	submit = Button(root, text="Submit", fg="Black", bg="Red", command=calc)
-	submit.grid(row=8, column=1)
-	root.mainloop()  
-re = tk.Tk() 
-re.title('voting system') 
+	submit = Button(re, text="Submit", fg="Black", bg="Red", command=calc)
+	submit.grid(row=9, column=1)
 button = tk.Button(re, text=' add a voter', width=25, command=avoter)
-button.pack()
-button = tk.Button(re,text='add a vote' , width=25,command=avoter)
-button.pack() 
+button.grid(row=0,column=0)
+butt = tk.Button(re,text='add a vote' , width=25,command=avoter)
+butt.grid(row=0,column=1)
 re.mainloop() 
